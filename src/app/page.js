b2dbd894431cb40948978e6304a0f52d3ded3c10
David BANITONGWA <davidbanitongwa@gmail.com>
2024-09-10 14:47:00 +0100

    update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,30 @@ import { BsArrowLeftShort } from "react-icons/bs";
 import { useState } from "react";
 import {AiFillEnvironment} from "react-icons/ai"
 import { BsSearch } from "react-icons/bs";
+import {RiDashboardFill} from "react-icons/ri"
 
 
 export default function Home() {
   const [open, setOpen] = useState(true)
+  const Menus = [
+    {title: "Dashboard" , icon: <RiDashboardFill/>},
+    {title: "Pages"},
+    {title: "Media", spacing : true},
+    {
+      title: "Projects",
+      submenu: true,
+      submenuItems:  [
+        {title: "Project 1"},
+        {title: "Project 2"},
+        {title: "Project 3"},
+      ]
+    },
+    {title: "Analytics"},
+    {title: "Inbox"},
+    {title: "Profile", spacing : true},
+    {title: "Setting"},
+    {title: "Logout"}
+  ]
   return (
     <div className="flex ">
       <div className={`bg-dark-purple h-screen p-5 pt-8 relative duration-300 ${open ? "w-72" : "w-20"}`}>
@@ -20,6 +40,17 @@ export default function Home() {
           <BsSearch className={`text-white text-lg block float-left cursor-pointer ${open && "mr-2"}`}/>
           <input type="search" placeholder="Recherche..." className={`text-base bg-transparent w-full text-white focus:outline-none ${!open && "hidden"}`}/>
         </div>
+
+        <ul className="pt-2">
+          {Menus.map((menu, index) => (
+            <li key={index} className={`text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md ${menu.spacing ? "mt-9" : "mt-2"}`}>
+              <span className="text-2xl block float-left">
+                {menu.icon}
+              </span>
+              <span className={`${!open && "hidden"} group-hover:block duration-300`}>{menu.title}</span>
+            </li>
+          ))}
+        </ul>
       </div>
 
 
