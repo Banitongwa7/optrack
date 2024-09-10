6c3534569686e90037f838a6798b40cf622d2507
David BANITONGWA <davidbanitongwa@gmail.com>
2024-09-10 13:01:10 +0100

    update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 import { BsArrowLeftShort } from "react-icons/bs";
 import { useState } from "react";
 import {AiFillEnvironment} from "react-icons/ai"
-
+import { BsSearch } from "react-icons/bs";
 
 
 export default function Home() {
@@ -12,8 +12,13 @@ export default function Home() {
       <div className={`bg-dark-purple h-screen p-5 pt-8 relative duration-300 ${open ? "w-72" : "w-20"}`}>
         <BsArrowLeftShort className={`bg-white text-dark-purple rounded-full text-3xl absolute -right-3 top-9 border border-dark-purple cursor-pointer ${!open && "rotate-180"}`} onClick={() => setOpen(!open)}/>
         <div className="inline-flex">
-          <AiFillEnvironment className={`bg-amber-300 rounded text-4xl block cursor-pointer float-left duration-500 ${open && "rotate-[360deg]"}`}/>
-          <h1 className={`text-white origin-left font-medium text-2xl duration-300 ml-5 ${!open && "scale-0"}`}>OpTrack</h1>
+          <AiFillEnvironment className={`bg-amber-300 rounded text-4xl block cursor-pointer float-left duration-500 mr-2 ${open && "rotate-[360deg]"}`}/>
+          <h1 className={`text-white origin-left font-medium text-2xl duration-300 ${!open && "scale-0"}`}>OpTrack</h1>
+        </div>
+
+        <div className={`flex items-center rounded-md bg-light-white mt-6 py-2 ${!open ? "px-2.5" : "px-4"}`}>
+          <BsSearch className={`text-white text-lg block float-left cursor-pointer ${open && "mr-2"}`}/>
+          <input type="search" placeholder="Recherche..." className={`text-base bg-transparent w-full text-white focus:outline-none ${!open && "hidden"}`}/>
         </div>
       </div>
 
