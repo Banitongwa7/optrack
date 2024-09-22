6749a04b5cbc72f76a67ea9d3c54cfb55721b094
David BANITONGWA <davidbanitongwa@gmail.com>
2024-09-22 13:21:37 +0100

    update route.js

--- src/app/api/opportunity/route.js
+++ src/app/api/opportunity/route.js
@@ -4,9 +4,26 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 
-export async function GET() {
-    const data = await prisma.opportunity.findMany()
-    return Response.json(data)
+export async function GET(req) {
+    const { searchParams } = new URL(req.url)
+    console.log("***************************")
+    console.log(searchParams)
+    console.log("***************************")
+    if (searchParams.has('id')) {
+        const id = searchParams.get('id')
+        const data = await prisma.opportunity.findMany(
+            {
+                where: {
+                    id: Number(id)
+                }
+            }
+        )
+        return Response.json(data)
+    }
+    else{
+        const data = await prisma.opportunity.findMany()
+        return Response.json(data)
+    }
 }
 
 export async function POST() {
